0ffe94806584a1b9ecc8d0aede6f17a82e97f9a5
LNikita19 <nikita.lilhore@pinnacle.in>
2025-09-03 13:05:06 +0530

    next

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,43 @@
+// const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // Required when deploying to Vercel with module federation
+//   output: "standalone",
+//   webpack(config: any, options: any) {
+//     const { isServer } = options;
+
+//     config.plugins.push(
+//       new NextFederationPlugin({
+//         name: "container",
+//         filename: "static/chunks/remoteEntry.js",
+//         remotes: {
+//           dashboard: `dashboard@http://localhost:3001/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
+//           remote2: `remote2@http://localhost:3002/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
+//         },
+//         exposes: {
+//           "./AuthContext": "./src/context/AuthContext.tsx",
+//         },
+//         shared: {
+//           react: { singleton: true, requiredVersion: false },
+//           "react-dom": { singleton: true, requiredVersion: false },
+
+//         },
+//       })
+//     );
+
+//     return config;
+//   },
+//   experimental: {
+//     outputFileTracingRoot: __dirname, // 👈 fixes multiple lockfiles warning
+//   },
+// };
+// module.exports = nextConfig;
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Required when deploying to Vercel with module federation
   output: "standalone",
-  webpack(config: any, options: any) {
+  webpack(config: { plugins: any[]; }, options: { isServer: any; }) {
     const { isServer } = options;
 
     config.plugins.push(
@@ -11,8 +45,8 @@ const nextConfig = {
         name: "container",
         filename: "static/chunks/remoteEntry.js",
         remotes: {
-          dashboard: `dashboard@http://localhost:3001/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
-          remote2: `remote2@http://localhost:3002/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
+          dashboard: `dashboard@https://dashboard-pi-kohl.vercel.app/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
+          remote2: `remote2@https://remote2-iota.vercel.app/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
         },
         exposes: {
           "./AuthContext": "./src/context/AuthContext.tsx",
@@ -20,7 +54,6 @@ const nextConfig = {
         shared: {
           react: { singleton: true, requiredVersion: false },
           "react-dom": { singleton: true, requiredVersion: false },
-
         },
       })
     );
@@ -28,7 +61,8 @@ const nextConfig = {
     return config;
   },
   experimental: {
-    outputFileTracingRoot: __dirname, // 👈 fixes multiple lockfiles warning
+    outputFileTracingRoot: __dirname,
   },
 };
+
 module.exports = nextConfig;
